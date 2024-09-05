8d040c9311afdd2b754fe1e73a63ba48afa3e9be
Martin A. Ortega <lynxestudio@outlook.com>
2024-09-05 01:36:49 -0600

    Adds source code to the project

---
+++ src/Samples.BitwiseCalc/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Samples.BitwiseCalc
+{
+    class Program
+    {
+        delegate int DelBinOP(int a, int b);
+        delegate int DelUriOP(int a);
+
+        static void Main(string[] args)
+        {
+        	Console.ForegroundColor = ConsoleColor.Green;
+            int option = 0;
+            string soption = "0";
+            do
+            {
+                try
+                {
+                    string[] options = { "& AND", "| OR", "^ Exclusive OR", "~ NOT", "<< Shift Left", ">> Shift Right" };
+                    Utilities.ShowMenu("BITWISE OPERATIONS", options);
+                    soption = Utilities.Scanf("Please enter your choice: ");
+                    Int32.TryParse(soption,out option);
+                    Utilities.PrintMessage("You choose " + options[option - 1]);
+                    switch (soption)
+                    {
+                        case "1":
+                            BinaryOp((a, b) => a & b);
+                            break;
+                        case "2":
+                            BinaryOp((a, b) => a | b);
+                            break;
+                        case "3":
+                            BinaryOp((a, b) => a ^ b);
+                            break;
+                        case "4":
+                            UnaryOp(a => ~a);
+                            break;
+                        case "5":
+                            ShiftOp((a, b) => a << b);
+                            break;
+                        case "6":
+                            ShiftOp((a, b) => a >> b);
+                            break;
+                        default:
+                            Console.WriteLine("\tPlease choose an option!");
+                            break;
+
+                    }
+                   
+                }
+                catch (Exception ex)
+                {
+                    Utilities.PrintMessage(ex.Message);
+                }
+            } while (!soption.Equals("0"));
+            
+        }
+
+        static void BinaryOp(DelBinOP del)
+        {
+        	int op1,op2;
+        	var soperator1 = Utilities.Scanf("Enter first integer:");
+            Int32.TryParse(soperator1, out op1);
+            var soperator2 = Utilities.Scanf("Enter second integer:");
+            Int32.TryParse(soperator2, out op2);
+            int resp = del(op1, op2);
+            Console.WriteLine();
+            Utilities.DisplayBinary(op1);
+            Utilities.DisplayBinary(op2);
+            Console.WriteLine("\t Result: ");
+            Utilities.DisplayBinary(resp);
+            Utilities.Pause();
+        }
+
+        static void ShiftOp(DelBinOP del)
+        {
+        	int op1,op2;
+        	var soperator1 = Utilities.Scanf("Enter first integer:");
+            Int32.TryParse(soperator1, out op1);
+            var soperator2 = Utilities.Scanf("Enter second integer:");
+            Int32.TryParse(soperator2, out op2);
+            int resp = del(op1, op2);
+            resp = del(op1, op2);
+            Console.WriteLine();
+            Utilities.DisplayBinary(op1);
+            Console.WriteLine("\t Result: ");
+            Utilities.DisplayBinary(resp);
+            Utilities.Pause();
+        }
+
+        static void UnaryOp(DelUriOP del)
+        {
+        	int op1;
+        	var soperator1 = Utilities.Scanf("Enter first integer:");
+            Int32.TryParse(soperator1, out op1);
+            int resp;
+            resp = del(op1);
+            Console.WriteLine();
+            Utilities.DisplayBinary(op1);
+			Console.WriteLine("\t Result: ");            
+            Utilities.DisplayBinary(resp);
+            Utilities.Pause();
+        }
+
+    }
+}
---
+++ src/Samples.BitwiseCalc/Utilities.cs
@@ -0,0 +1,88 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Util.cs" company="apifiedsignature">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Samples.BitwiseCalc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    internal static class Utilities
+    {
+        internal static void ShowMenu(String title,string[] collection)
+        {
+            Console.Clear();
+
+            int width = Console.WindowWidth - 4;
+            int i = 0;
+            Console.WriteLine("+" + new string('-', width - 2) + "+");
+            Console.WriteLine("|" + CenterText(title, width - 2) + "|");
+            Console.WriteLine("|" + new string(' ', width - 2) + "|");
+            foreach (var item in collection)
+            {
+                i++;
+                Console.WriteLine("| " + AlignLeftText(i + ")  " + item, width - 3) + "|");   
+            }
+            Console.WriteLine("| " + AlignLeftText("0. Exit", width - 3) + "|");
+            Console.WriteLine("|" + new string(' ', width - 2) + "|");
+            Console.WriteLine("+" + new string('-', width - 2) + "+");
+        }
+
+        internal static string Scanf(string message)
+        {
+            Console.Write("\t[ " + message + " ]\t");
+            return Console.ReadLine();
+        }
+
+        internal static void Pause()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\tPress [Enter] to continue...");
+            Console.ReadLine();
+        }
+
+        internal static void PrintMessage(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t+--------------------------------------+");
+            Console.WriteLine("\t|" + message);
+            Console.WriteLine("\t+--------------------------------------+");
+            Console.WriteLine();
+        }
+
+        static string CenterText(string text, int width)
+        {
+            if (text.Length >= width)
+                return text.Substring(0, width); // Truncate if text is longer than width
+
+            int spaces = width - text.Length;
+            int padLeft = spaces / 2;
+            int padRight = spaces - padLeft;
+
+            return new string(' ', padLeft) + text + new string(' ', padRight);
+        }
+
+        static string AlignLeftText(string text, int width)
+        {
+            if (text.Length >= width)
+                return text.Substring(0, width); // Truncate if text is longer than width
+
+            int spaces = width - text.Length;
+            return text + new string(' ', spaces);
+        }
+
+        internal static void DisplayBinary(int i)
+        {
+        	int width = Console.WindowWidth - 4;
+        	string s = String.Format("{0} = {1}", i, Convert.ToString(i, 2).PadLeft(32, '0'));
+        	Console.WriteLine("|" + CenterText(s, width - 2) + "|");
+        }
+    }
+}
